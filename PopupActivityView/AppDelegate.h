85d9abeb62b0e2e6523f8203e84bf5a8d45b8b7a
Canymebee <Canymebee@163.com>
2014-12-17 19:46:22 +0800

    Initial Commit

---
+++ PopupActivityView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopupActivityView
+//
+//  Created by Rain on 14/12/17.
+//  Copyright (c) 2014年 Canymebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopupActivityView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopupActivityView
+//
+//  Created by Rain on 14/12/17.
+//  Copyright (c) 2014年 Canymebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
